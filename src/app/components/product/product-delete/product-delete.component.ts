0330d74b93a0a9cfadd98e26e0ba28296c324da0
Alex Cozer <acabrantes@hotmail.com>
2020-06-10 18:42:39 -0300

    Curso Finalizado!

---
+++ src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,44 @@
+import { Product } from './../product.model';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Route } from '@angular/compiler/src/core';
+
+@Component({
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
+})
+export class ProductDeleteComponent implements OnInit {
+
+  product: Product = {
+    name: '',
+    price: null
+  }
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  // sinal de + converte para númerico
+  ngOnInit(): void {
+    const id = +this.route.snapshot.paramMap.get('id')
+    this.productService.readById(id).subscribe(product => {
+      this.product = product
+    });
+  }
+
+  deleteProduct(): void {
+    this.productService.delete(this.product.id).subscribe(() => {
+      this.productService.showMessage('Produto excluído com sucesso!')
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+
+}
